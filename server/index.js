b2ce9653c0394f179075c31311de46cd8d864e36
kanchan <kanchankotkar001@gmail.com>
2023-01-07 13:54:15 +0530

    signup & login api added

--- server/index.js
+++ server/index.js
@@ -9,49 +9,151 @@ const app = express();
 app.use(express.json());
 
 
-const PORT=process.env.PORT ||5000;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGODB_URL,()=>{
+mongoose.connect(process.env.MONGODB_URL, () => {
     console.log('connected to mongodb')
 })
 
 
 //api routes start here
 
-app.post('/signup',async(req,res)=>{
-   
-   const {name,email,phone,password,role} =req.body;
+app.post('/signup', async (req, res) => {
 
-//    if(!name ||!phone||!email||!password||!role){
-//     return res.json({
-//         success:false
-//     })
-//    }
+    const { name, email, phone, password, role } = req.body;
 
-   const user = new User({
-    name:name,
-    email:email,
-    phone:phone,
-    password:password,
-    role:role
-   })
 
-   const savedUser = await user.save();
-   res.json({
-    success:true,
-    message:"user created successfully",
-    data:savedUser
-   })
+    // if (!name) {
+    //     return res.json({
+    //         success: false,
+    //         message: "name is required"
+
+    //     })
+    // }
+    // if (!phone) {
+    //     return res.json({
+    //         success: false,
+    //         message: "phone is required"
+
+    //     })
+    // }
+
+    // if (!email) {
+    //     return res.json({
+    //         success: false,
+    //         message: "email is required"
+
+    //     })
+    // }
+    // if (!phone) {
+    //     return res.json({
+    //         success: false,
+    //         message: "name is required"
+
+    //     })
+    // }
+    // if (!password) {
+    //     return res.json({
+    //         success: false,
+    //         message: "password is required"
+
+    //     })
+    // }
+    // if (!role) {
+    //     return res.json({
+    //         success: false,
+    //         message: "role is required"
+
+    //     })
+    // }OR
+
+
+    // validation to check if all fields are filled starts here
+
+    const emptyFields = [];
+    if (!name) emptyFields.push('name');
+    if (!email) emptyFields.push('email');
+    if (!phone) emptyFields.push('phone');
+    if (!password) emptyFields.push('password');
+    if (!role) emptyFields.push('role');
+
+    if (emptyFields.length > 0) {
+        return res.json({
+            success: false,
+            message: `${emptyFields.join(',')} are required`
+        })
+    }
+    // validation to check if all fields are filled ends here
+
+    // validation to check if email already exists start here
+    const existingUser = await new User.findOne({ email: email });
+    if (existingUser) {
+        return res.json({
+            success: false,
+            message: "email already exists"
+        })
+    }
+    // validation to check if email already exists ends here
+
+    // validation to check if phone already exists satrt here
+    const existingUserPhone = await new User.findOne({ phone: phone });
+    if (existingUserPhone) {
+        res.json({
+            success: false,
+            message: "phone is already exists"
+        })
+    }
+
+    // validation to check if phone already exists ends here
+
+
+    const user = new User({
+        name: name,
+        email: email,
+        phone: phone,
+        password: password,
+        role: role
+    })
+
+    const savedUser = await user.save();
+    res.json({
+        success: true,
+        message: "user created successfully",
+        data: savedUser
+    })
 
 })
 
+app.post('/login',async(req,res)=>{
+    const {email,password} = req.body;
 
+    if(!email || !password){
+        return res.json({
+            success:false,
+            message:"Email and password are required"
+        })
+    }
+    const existingUser = await User.findOne({email:email,password:password});
+      if(existingUser){
+        return res.json({
+            success:true,
+            message:"Login Successfully",
+            data:existingUser 
+        })
+      } 
+      else{
+        return res.json({
+            success:false,
+            message:"Invalid email or password"
+        })
+      }
+})
 
 //api routes ends here
 
 
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log("server is running on port 5000")
 })
